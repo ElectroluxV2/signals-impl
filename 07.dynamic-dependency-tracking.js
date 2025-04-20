ddacd6c1388399d0fc05e27ee39dd7b39a541404
Mateusz Budzisz <electroluxv2@gmail.com>
2025-04-20 18:15:26 +0200

    3

---
+++ 07.dynamic-dependency-tracking.js
@@ -0,0 +1,103 @@
+export const signalsSystem = {
+  currentSubscriber: undefined,
+};
+
+export const signal = (initialValue) => {
+  let value = initialValue;
+  const subscribers = new Set();
+
+  return {
+    get: () => {
+      if (signalsSystem.currentSubscriber) {
+        const temp = signalsSystem.currentSubscriber.notify;
+        signalsSystem.currentSubscriber.addDependency(() => subscribers.delete(temp));
+        subscribers.add(temp);
+      }
+
+      return value;
+    },
+    set: (newValue) => {
+      value = newValue;
+
+      for (const subscriber of subscribers) {
+        subscriber();
+      }
+    },
+  }
+}
+
+export const computed = (computation) => {
+  let stale = true;
+  let cachedValue = undefined;
+  const subscribers = new Set();
+  const dependencies = new Set();
+
+  const markAsStale = () => {
+    stale = true;
+    // Notify that value could have changed
+    for (const subscriber of subscribers) {
+      subscriber();
+    }
+  };
+
+  return {
+    get: () => {
+      if (signalsSystem.currentSubscriber) {
+        const temp = signalsSystem.currentSubscriber.notify;
+        signalsSystem.currentSubscriber.addDependency(() => subscribers.delete(temp));
+        subscribers.add(temp);
+      }
+
+      if (!stale) {
+        return cachedValue;
+      }
+
+      for (const removeNotifySubscription of dependencies) {
+        removeNotifySubscription(markAsStale);
+      }
+      dependencies.clear();
+
+      const prev = signalsSystem.currentSubscriber;
+      signalsSystem.currentSubscriber = {
+        notify: markAsStale,
+        addDependency: (removeDependencyFn) => dependencies.add(removeDependencyFn),
+      };
+      cachedValue = computation();
+      stale = false;
+      signalsSystem.currentSubscriber = prev;
+
+      return cachedValue;
+    },
+  }
+}
+
+export const effect = (effectFn) => {
+  const dependencies = new Set();
+  let pending = false;
+
+  const scheduleEffect = () => {
+    if (pending) return;
+    pending = true;
+    queueMicrotask(runEffect);
+  }
+
+  const runEffect = () => {
+
+    for (const removeNotifySubscription of dependencies) {
+      removeNotifySubscription(runEffect);
+    }
+    dependencies.clear();
+
+    const prev = signalsSystem.currentSubscriber;
+    signalsSystem.currentSubscriber = {
+      notify: scheduleEffect,
+      addDependency: (removeDependencyFn) => dependencies.add(removeDependencyFn),
+    };
+    effectFn();
+    pending = false;
+    signalsSystem.currentSubscriber = prev;
+  }
+
+  // Effect is guaranteed to run at least once
+  runEffect();
+}
---
+++ 07.static-dependency-tracking.sample.js
@@ -0,0 +1,30 @@
+import { computed, effect, signal } from './06.scheduled-effect.js';
+
+console.log('Static dependency tracking');
+const reactiveVariable1 = signal('var-1 [0]');
+const reactiveVariable2 = signal('var-2 [0]');
+
+const shouldUse1 = signal(true);
+
+const reactiveConditionalComputed = computed(() => {
+  const value = shouldUse1.get() ? reactiveVariable1.get() : reactiveVariable2.get();
+  return `Computed value: ${value}`;
+});
+
+effect(() =>
+  console.log(reactiveConditionalComputed.get()),
+);
+
+shouldUse1.set(false);
+await new Promise(r => setTimeout(r, 0));
+console.log('Updates to `reactiveVariable1` below, should not trigger effect');
+
+await new Promise(r => setTimeout(r, 1));
+reactiveVariable1.set('var-1 [1]');
+
+await new Promise(r => setTimeout(r, 1));
+reactiveVariable1.set('var-1 [2]');
+
+await new Promise(r => setTimeout(r, 1));
+console.log('But changes to `reactiveVariable2` should');
+reactiveVariable2.set('var-2 [1]');
